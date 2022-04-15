bc1ef64842c2386ca718b1d67d7fc98c7070f49f
Sanzharmoldobekov <103410435+Sanzharmoldobekov@users.noreply.github.com>
2022-04-15 13:43:57 +0600

    Update 5_zadacha.py

--- 5_zadacha.py
+++ 5_zadacha.py
@@ -5,3 +5,19 @@ a1=1
 b='7'
 print(a1*b)
 print (len(a))
+
+# Задача 3
+# Здесь замешаны разные типы данных.
+# Напишите программу, которая берёт массив данных spisok2 и выводит только те элементы из spisok_2, которых нет в spisok_1.
+# spisok_1 = ('Lamborgini', 17, '4456', 2020, 'Paris', 'USA', 11, 23)
+# spisok_2 = ('Ferrari', 17, 4456, 2021, 'Paris', 'UK', 777, 23)
+
+spisok_1 = ('Lamborgini', 17, '4456', 2020, 'Paris', 'USA', 11, 23)
+spisok_2 = ('Ferrari', 17, 4456, 2021, 'Paris', 'UK', 777, 23)
+x=[]
+for i in spisok_1 :
+	for j in spisok_2:
+		if i==j:
+			x.append(i)
+			break
+print(x)
